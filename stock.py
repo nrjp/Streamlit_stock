88183e9a1ecd9d833628d18693e6b97c738556ef
Niraj Patharkar <nirajpathrkar@gmail.com>
2022-12-30 00:31:21 +0530

    added_options

--- stock.py
+++ stock.py
@@ -1,34 +1,52 @@
 import yfinance as yf
 import streamlit as st
-import pandas as  pd
-
+import pandas as pd
 
 st.write("""
-### Simple Stock Information App """)
+## Simple Stock Information App """)
 
-#Get stock symbol from user
-X = st.text_input("Stock Symbol")
-#symbol
-symbol = X
-
+# Get stock symbol from user
+symbol = st.text_input("Stock Symbol")
 Data = yf.Ticker(symbol)
 info = Data.info
+if type(info) == dict:
+    # check if variable having value
+    # get some important values
+    st.write("Name:  {}  ".format(info.get('shortName')))
+    st.write("Sector:  {}".format(info.get('sector')))
+    st.write("Country:  {}".format(info.get('country')))
+    st.write("Industry:  {}".format(info.get('industry')))
+    st.write("Total Revenue:  {}".format(info.get('totalRevenue')))
+    st.write("Pre Market Price:  {}".format(info.get('preMarketPrice')))
+    st.write("Open:  {}".format(info.get('open')))
+    st.write("Fifty Two Week High:  {}".format(info.get('fiftyTwoWeekHigh')))
+    st.write("fifty Two Week Low:  {}".format(info.get('fiftyTwoWeekLow')))
 
+    # closing price graph
+    tickerDf = Data.history(period='1d', start='2020-01-01', end='2022-11-30')
+    st.write("""
+    ### Closing Price """)
 
-# app
-st.write("""
-shows are the stock **closing price** and **Volume** of {}""".format(info.get('longBusinessSummary')[0:22]))
+    # volume graph
+    st.line_chart(tickerDf.Close)
+    st.write("""
+    ### Volume """)
+    st.line_chart(tickerDf.Volume)
 
-tickerDf = Data.history(period='1d',start='2020-01-01',end='2022-11-30')
-st.write("""
-### Closing Price """)
-st.line_chart(tickerDf.Close)
-st.write("""
-### Volume """)
-st.line_chart(tickerDf.Volume)
-## other information
-option = st.selectbox(
-    'Seleted from following',
-    ('History', 'Closing Price', 'Volume'))
-st.write(Data.history(period="max"))
-
+    ## information in dataframe
+    option = st.selectbox(
+        'Seleted from following',
+        ('History', 'Closing Price', 'Volume'))
+    if option == 'History':
+        option = st.selectbox(
+            'Seleted from following',
+            ('1 month', '4 Months', '1 Year', '5 Year', 'max'))
+        st.write(Data.history(period=option))
+    elif option == "Closing Price":
+        st.write("""
+        ### Closing Price """, tickerDf.Close)
+    else:
+        st.write("""
+            ### Closing Price """, tickerDf.Volume)
+else:
+    st.write("""### Please input Valid Stock symbol """)
