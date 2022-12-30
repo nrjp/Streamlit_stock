04673caa6f105457ec39d04081d33d5602d29a52
Niraj Patharkar <nirajpathrkar@gmail.com>
2022-12-30 16:39:13 +0530

    historical_data

--- stock.py
+++ stock.py
@@ -1,6 +1,5 @@
 import yfinance as yf
 import streamlit as st
-import pandas as pd
 
 st.write("""
 ## Simple Stock Information App """)
@@ -9,18 +8,34 @@ st.write("""
 symbol = st.text_input("Stock Symbol")
 Data = yf.Ticker(symbol)
 info = Data.info
+
 if type(info) == dict:
     # check if variable having value
     # get some important values
-    st.write("Name:  {}  ".format(info.get('shortName')))
-    st.write("Sector:  {}".format(info.get('sector')))
-    st.write("Country:  {}".format(info.get('country')))
-    st.write("Industry:  {}".format(info.get('industry')))
-    st.write("Total Revenue:  {}".format(info.get('totalRevenue')))
-    st.write("Pre Market Price:  {}".format(info.get('preMarketPrice')))
-    st.write("Open:  {}".format(info.get('open')))
-    st.write("Fifty Two Week High:  {}".format(info.get('fiftyTwoWeekHigh')))
-    st.write("fifty Two Week Low:  {}".format(info.get('fiftyTwoWeekLow')))
+    if st.button('More Information'):
+        st.write("Name:  {}  ".format(info.get('shortName')))
+        st.write("Sector:  {}".format(info.get('sector')))
+        st.write("Country:  {}".format(info.get('country')))
+        st.write("Industry:  {}".format(info.get('industry')))
+        st.write("Total Revenue:  {}".format(info.get('totalRevenue')))
+        st.write("Pre Market Price:  {}".format(info.get('preMarketPrice')))
+        st.write("Open:  {}".format(info.get('open')))
+        st.write("Current Price:  {}".format(info.get('currentPrice')))
+        st.write("Fifty Two Week High:  {}".format(info.get('fiftyTwoWeekHigh')))
+        st.write("fifty Two Week Low:  {}".format(info.get('fiftyTwoWeekLow')))
+        st.write("Website:  {}".format(info.get('website')))
+
+    else:
+        pass
+
+    if st.button('Balance sheet'):
+        st.write(Data.balancesheet)
+
+    if st.button('Cashflow sheet'):
+        st.write(Data.cashflow)
+
+    if st.button('Financials sheet'):
+        st.write(Data.financials)
 
     # closing price graph
     tickerDf = Data.history(period='1d', start='2020-01-01', end='2022-11-30')
@@ -36,17 +51,15 @@ if type(info) == dict:
     ## information in dataframe
     option = st.selectbox(
         'Seleted from following',
-        ('History', 'Closing Price', 'Volume'))
-    if option == 'History':
-        option = st.selectbox(
-            'Seleted from following',
-            ('1 month', '4 Months', '1 Year', '5 Year', 'max'))
-        st.write(Data.history(period=option))
+        ('Historical Data', 'Closing Price', 'Volume'))
+    if option == 'Historical Data':
+        st.write(Data.history())
     elif option == "Closing Price":
         st.write("""
         ### Closing Price """, tickerDf.Close)
     else:
         st.write("""
-            ### Closing Price """, tickerDf.Volume)
+            ### Volume """, tickerDf.Volume)
+
 else:
     st.write("""### Please input Valid Stock symbol """)
